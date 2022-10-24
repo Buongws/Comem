fd923fb565a0571bcb463ec2e031547f95f06883
Buong <clonedopdz@gmail.com>
2022-10-25 01:35:45 +0700

    create json data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import data from "./utils/data.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { TrangDiem } = data;
+
+  console.log(data);
+
+  new Promise((resolve, reject) => {
+    setTimeout(() => resolve("data"), Math.random() * 200);
+  });
+  return <div className="App"></div>;
 }
 
 export default App;
